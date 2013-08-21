5434fb57dd0a47400ad2aa3126857dae19c64419
Olivier SCHNEIDER <oliwan3dfx@gmail.com>
2013-08-21 10:38:16 +0200

    Add realtime services

--- app/scripts/controllers/todos.js
+++ app/scripts/controllers/todos.js
@@ -1,8 +1,37 @@
 'use strict';
 
 angular.module('todoApp')
-    .controller('TodosCtrl', function ($scope, NodeStorage) {
-        $scope.todos = NodeStorage.query();
+    .controller('TodosCtrl', function ($scope, socket) {
+        $scope.todos = [];
+
+        socket.on("todos", function (data) {
+            $scope.todos = [];
+            data.forEach(function (element) {
+                if (element)
+                    $scope.todos.push(element);
+            });
+        });
+
+        socket.on("new todo", function (todo) {
+            console.log("new todo", todo);
+            $scope.todos.push(todo);
+        });
+
+        socket.on("delete todo", function (todo) {
+            $scope.applyOnTodos(function (element) {
+                if (element.id != todo.id)
+                    return element;
+                return false;
+            });
+        });
+
+        socket.on("update todo", function (todo) {
+            $scope.applyOnTodos(function (element) {
+                if (element.id == todo.id)
+                    return todo;
+                return element;
+            });
+        });
 
         $scope.addTodo = function () {
             if (!$scope.newTodo ||
@@ -14,18 +43,11 @@ angular.module('todoApp')
                 title: $scope.newTodo.trim(),
                 completed: false
             };
-            NodeStorage.save(todo, function(data){
-                $scope.todos.push(data);
-                $scope.newTodo = null;
-            });
+            socket.emit('new todo', todo);
         };
 
         $scope.removeTodo = function (todo) {
-            var index = $scope.todos.indexOf(todo);
-            $scope.todos.splice(index, 1);
-            NodeStorage.delete({
-                id: todo.id
-            });
+            socket.emit('delete todo', todo);
         };
 
         $scope.startEditing = function (todo) {
@@ -36,7 +58,7 @@ angular.module('todoApp')
         $scope.doneEditing = function () {
             $scope.backupTodo = null;
             $scope.editingTodo.title = $scope.editingTodo.title.trim();
-            NodeStorage.update($scope.editingTodo);
+            socket.emit('update todo', $scope.editingTodo);
             $scope.editingTodo = null;
         };
 
@@ -47,7 +69,7 @@ angular.module('todoApp')
         };
 
         $scope.onCompleted = function (todo) {
-            NodeStorage.update(todo);
+            socket.emit('update todo', todo);
         };
 
         $scope.filter = function (completed) {
@@ -58,5 +80,15 @@ angular.module('todoApp')
 
         $scope.removeFilter = function () {
             $scope.completionFilter = null;
+        };
+
+        $scope.applyOnTodos = function (fn) {
+            var newTodos = [];
+            $scope.todos.forEach(function (element) {
+                element = fn(element);
+                if (element)
+                    newTodos.push(element);
+            });
+            $scope.todos = newTodos;
         };
     });
--- server.js
+++ server.js
@@ -1,17 +1,48 @@
 var express = require('express');
+var io = require('socket.io');
+var http = require('http');
 var todos = require('./server/todos.js');
+var todoDb = require('./server/todo-db.js');
+
 var app = express();
 var port = 3000;
+
+var server = http.createServer(app);
+server.listen(port);
+io = io.listen(server);
 
 app.use(express.bodyParser());
 
 app.use(express.static('app'));
 
+// rest services
 app.get('/todos', todos.get);
 app.get('/todos/:id', todos.getById);
 app.post('/todos', todos.post);
 app.put('/todos/:id', todos.put);
 app.delete('/todos/:id', todos.delete);
 
-app.listen(port);
-console.log("Server listening on port "+port);
+// realtime services
+io.sockets.on('connection', function (socket) {
+    socket.emit('todos', todoDb.get());
+
+    socket.on('new todo', function (todo) {
+        todoDb.persist(todo);
+        socket.emit('new todo', todo);
+        socket.broadcast.emit('new todo', todo);
+    });
+
+    socket.on('delete todo', function (todo) {
+        todoDb.delete(todo);
+        socket.emit('delete todo', todo);
+        socket.broadcast.emit('delete todo', todo);
+    });
+
+    socket.on('update todo', function (todo) {
+        todoDb.update(todo);
+        socket.emit('update todo', todo);
+        socket.broadcast.emit('update todo', todo);
+    });
+});
+
+console.log("Server listening on port " + port);
